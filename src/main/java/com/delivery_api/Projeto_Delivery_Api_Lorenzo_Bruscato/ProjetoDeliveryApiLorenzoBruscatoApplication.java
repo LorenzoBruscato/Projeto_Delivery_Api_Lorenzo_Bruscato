4e02855fc79e71a4c3ac1e4cae95b02484856041
LorenzoBruscato <lorenzobruscato6@gmail.com>
2025-10-30 14:10:41 -0300

    Initial commit

---
+++ src/main/java/com/delivery_api/Projeto_Delivery_Api_Lorenzo_Bruscato/ProjetoDeliveryApiLorenzoBruscatoApplication.java
@@ -0,0 +1,13 @@
+package com.delivery_api.Projeto_Delivery_Api_Lorenzo_Bruscato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDeliveryApiLorenzoBruscatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDeliveryApiLorenzoBruscatoApplication.class, args);
+	}
+
+}
