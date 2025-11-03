4d9a1b335c88ffda74dcafff8c514ef3af8d7b35
LorenzoBruscato <lorenzobruscato6@gmail.com>
2025-11-03 01:10:13 -0300

    Initial commit

---
+++ src/test/java/com/delivery_api/Projeto_Delivery_Api_Lorenzo_Bruscato/ProjetoDeliveryApiLorenzoBruscatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery_api.Projeto_Delivery_Api_Lorenzo_Bruscato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDeliveryApiLorenzoBruscatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
